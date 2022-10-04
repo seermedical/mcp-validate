923f46d6ceec422b5800900a13a40293051c2bd0
Dominique Eden <dominique@seermedical.com>
2022-10-04 16:26:19 +1100

    Add in function to take and transform input JSON

--- src/generate_outputs.py
+++ src/generate_outputs.py
@@ -7,6 +7,23 @@ Online tool: epipick.org
 """
 
 import numpy as np
+from typing import Mapping
+
+from get_input_values import get_flag_value
+
+FLAG_1_KEYS = ['']
+FLAG_2_KEYS = ['']
+FLAG_3_KEYS = ['']
+FLAG_4_KEYS = ['']
+FLAG_5_KEYS = ['']
+FLAG_6_KEYS = ['']
+
+FLAG_1_KEYWORDS = ['']
+FLAG_2_KEYWORDS = ['']
+FLAG_3_KEYWORDS = ['']
+FLAG_4_KEYWORDS = ['']
+FLAG_5_KEYWORDS = ['']
+FLAG_6_KEYWORDS = ['']
 
 
 def has_undefined_values(input_array: np.ndarray, threshold: int = 3):
@@ -29,10 +46,24 @@ def has_positive_values(input_array: np.ndarray, threshold: int = 0):
     return np.count_nonzero(input_array) > threshold
 
 
-def generate_output(input_array: np.ndarray):
-    """Predicts diagnosis of each patient based on a set of inputs.
+def transform_input(input_dict: Mapping[str, Mapping[str, str]]):
+    """Takes dictionary of patient's responses to survey questions
+    and transforms to One Hot Encoded matrix."
 
-    Input:
+    Args:
+        input_dict (dict): Dictionary where keys represent a patient,
+        enclosing another dictionary of key (question), value (answer) pairs.
+        N.b. If no value (answer), to a key (question), an empty string is expected.
+        Example:
+            {
+                'patient_1': {
+                    'How long do your seizures last?': 'a few seconds',
+                    'Describe what happens during your seizures.': '',
+                    ...
+                }
+            }
+      
+    Returns:
         np.ndarray: Input array where rows represent each patient, and columns
         represent each input (i.e. question). Inputs are as follows:
             input_1 - Did skin turn pale before event?
@@ -56,18 +87,60 @@ def generate_output(input_array: np.ndarray):
 
             Elements are represented as NaN = no data, 0 = 'no', or 1 = 'yes'.
             Example:
-            # +--------+--------+--------+--------+--------+--------+--------+------+----------------+----------+---------+-------+--------------+
-            # | flag_1 | flag_2 | flag_3 | flag_4 | flag_5 | flag_6 | lesion | lips | night_seizures | onset_21 | staring | jerks | tonic_clonic |
-            # +--------+--------+--------+--------+--------+--------+--------+------+----------------+----------+---------+-------+--------------+
-            # | NaN    | NaN    | NaN    | NaN    | NaN    | NaN    | 1      | 0    | 0              | 0        | 0       | 0     | 0            |
-            # +--------+--------+--------+--------+--------+--------+--------+------+----------------+----------+---------+-------+--------------+
-            # | 1      | 1      | 1      | 0      | 0      | 0      | 1      | 0    | 0              | 0        | 0       | 0     | 0            |
-            # +--------+--------+--------+--------+--------+--------+--------+------+----------------+----------+---------+-------+--------------+
-            # | 1      | 1      | 1      | 0      | 0      | 0      | 0      | 0    | 0              | 0        | 1       | 1     | 0            |
-            # +--------+--------+--------+--------+--------+--------+--------+------+----------------+----------+---------+-------+--------------+
+                # +--------+--------+--------+--------+--------+--------+--------+------+----------------+----------+---------+-------+--------------+
+                # | flag_1 | flag_2 | flag_3 | flag_4 | flag_5 | flag_6 | lesion | lips | night_seizures | onset_21 | staring | jerks | tonic_clonic |
+                # +--------+--------+--------+--------+--------+--------+--------+------+----------------+----------+---------+-------+--------------+
+                # | NaN    | NaN    | NaN    | NaN    | NaN    | NaN    | 1      | 0    | 0              | 0        | 0       | 0     | 0            |
+                # +--------+--------+--------+--------+--------+--------+--------+------+----------------+----------+---------+-------+--------------+
+                # | 1      | 1      | 1      | 0      | 0      | 0      | 1      | 0    | 0              | 0        | 0       | 0     | 0            |
+                # +--------+--------+--------+--------+--------+--------+--------+------+----------------+----------+---------+-------+--------------+
+                # | 1      | 1      | 1      | 0      | 0      | 0      | 0      | 0    | 0              | 0        | 1       | 1     | 0            |
+                # +--------+--------+--------+--------+--------+--------+--------+------+----------------+----------+---------+-------+--------------+
+    """
+    patients = input_dict.keys()
+
+    # Init (transformed) One Hot Encoded input array
+    input_array = np.zeros(len(patients), 13)
+
+    for idx, patient in enumerate(patients):
+        patient_dict = input_dict[patient]
+
+        input_array[idx, 0] = get_flag_value(
+            input_dict={key: patient_dict[key]
+                        for key in FLAG_1_KEYS},
+            list_of_keywords=FLAG_1_KEYWORDS)
+        input_array[idx, 1] = get_flag_value(
+            {key: patient_dict[key]
+             for key in FLAG_2_KEYS},
+            list_of_keywords=FLAG_2_KEYWORDS)
+        input_array[idx, 2] = get_flag_value(
+            {key: patient_dict[key]
+             for key in FLAG_3_KEYS},
+            list_of_keywords=FLAG_3_KEYWORDS)
+        input_array[idx, 3] = get_flag_value(
+            {key: patient_dict[key]
+             for key in FLAG_4_KEYS},
+            list_of_keywords=FLAG_4_KEYWORDS)
+        input_array[idx, 4] = get_flag_value(
+            {key: patient_dict[key]
+             for key in FLAG_5_KEYS},
+            list_of_keywords=FLAG_5_KEYWORDS)
+        input_array[idx, 5] = get_flag_value(
+            {key: patient_dict[key]
+             for key in FLAG_6_KEYS},
+            list_of_keywords=FLAG_6_KEYWORDS)
+
+    return input_array.astype(int)
 
 
-    Output:
+def generate_output(input_array: np.ndarray):
+    """Predicts diagnosis of each patient based on a set of inputs.
+
+    Args:
+        np.ndarray: Input array where rows represent each patient, and columns
+        represent each input (i.e. question). See to transform_input().
+
+    Returns:
         np.ndarray: Output array where rows represent patients and columns represent
         predicted diagnosis (i.e. output classes). Outputs are as follows:
         output_1 - Non-epileptic paroxysmal event
