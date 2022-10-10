f0979d709d45565137da9e2d7414e7dc33a91df6
Dominique Eden <dominique@seermedical.com>
2022-10-10 14:10:25 +1100

    Use pytest lazy fixture to parametrize using pytext fixtures

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,27 +1,25 @@
 import pytest
 
-from src.generate_inputs import get_input_value, FLAG_1_KEYWORDS
+from src.generate_inputs import InputFilter, FLAG_KEYS_BEFORE, FLAG_6_KEYWORDS_BEFORE
 
 
 @pytest.fixture
 def patient_1_dict():
     return {
         'What other things do you experience right before or at the beginning of a seizure?':
-        'Response 1',
+        '',
         'Please describe what you feel right before or at the beginning of a seizure.':
-        'Response 2',
+        'I get a headache.',
         'Please specify other warning.':
         '',
         'Please specify other injuries.':
         '',
         'What injuries have you experienced during a seizures':
-        'Response 3',
+        '',
         'Please specify other symptoms.':
-        'Response 4',
+        '',
         'Describe what happens during your seizures.':
-        'Response 5',
-        'Please describe what you feel right before or at the beginning of a seizure.':
-        'Response 6',
+        '',
         'How long do your seizures last?':
         'a few seconds'
     }
@@ -31,34 +29,60 @@ def patient_1_dict():
 def patient_2_dict():
     return {
         'What other things do you experience right before or at the beginning of a seizure?':
-        'Response 1',
+        '',
         'Please describe what you feel right before or at the beginning of a seizure.':
-        'Response 2',
+        'I feel pain, then I fall.',
         'Please specify other warning.':
         '',
         'Please specify other injuries.':
         '',
         'What injuries have you experienced during a seizure?s':
-        'Response 3',
+        '',
         'Please specify other symptoms.':
-        'Response 4',
+        '',
         'Describe what happens during your seizures.':
-        'Response 5',
+        '',
+        'How long do your seizures last?':
+        'a few seconds'
+    }
+
+
+@pytest.fixture
+def patient_3_dict():
+    return {
+        'What other things do you experience right before or at the beginning of a seizure?':
+        '',
         'Please describe what you feel right before or at the beginning of a seizure.':
-        'Response 6',
+        '',
+        'Please specify other warning.':
+        '',
+        'Please specify other injuries.':
+        '',
+        'What injuries have you experienced during a seizure?s':
+        '',
+        'Please specify other symptoms.':
+        '',
+        'Describe what happens during your seizures.':
+        '',
         'How long do your seizures last?':
         'a few seconds'
     }
 
 
 class TestGetInputValues:
-    """_summary_
+    """Tests function to check keywords in free text answer,
+    and return True, False, or None based on a positive, negative, or
+    no answer respectively. 
     """
-    @pytest.mark.parametrize("patient_dict", "expected_boolean",
-                             [(patient_1_dict, True)],
-                             [(patient_2_dict, False)])
-    def test_get_input_values(self, patient_dict, expected_boolean):
-        result = get_input_value(input_dict=patient_dict,
-                                 list_of_keywords=FLAG_1_KEYWORDS)
+
+    @pytest.mark.parametrize("mock_patient_dict, expected_boolean",
+                             [(pytest.lazy_fixture("patient_1_dict"), False),
+                              (pytest.lazy_fixture("patient_2_dict"), True),
+                              (pytest.lazy_fixture("patient_3_dict"), None)])
+    def test_get_input_values(self, mock_patient_dict, expected_boolean):
+        input_filter = InputFilter(patient_dict=mock_patient_dict)
+        result = input_filter.get_flag_value(
+            list_of_keys=FLAG_KEYS_BEFORE,
+            list_of_keywords=FLAG_6_KEYWORDS_BEFORE)
 
         assert result == expected_boolean
