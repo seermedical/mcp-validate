334aaa1cee3414b8cf78b2dbcf43508e36ed7808
Dominique Eden <dominique@seermedical.com>
2022-10-24 14:27:04 +1100

    Test all combinations of keywords for input 1 [WIP]

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,12 +1,8 @@
 import numpy as np
 import pytest
+import spacy
 
-from src.generate_inputs import (
-    InputFilter,
-    transform_input,
-    BEFORE_EVENT_QUESTIONS,
-    FLAG_6_KEYWORDS_BEFORE,
-)
+from src.generate_inputs import matches_criteria, KEYWORDS_DICT
 
 
 def mock_input_dict_template(
@@ -25,7 +21,7 @@ def mock_input_dict_template(
             "Please describe what you feel right before or at the beginning of a seizure.": response_1,
             "Please specify other warning.": response_2,
             "Please specify other injuries.": response_3,
-            "What injuries have you experienced during a seizures": response_4,
+            "Which warnings do you get before you have a seizure?": response_4,
             "Please specify other symptoms.": response_5,
             "Describe what happens during your seizures.": response_6,
             "How long do your seizures last?": response_7,
@@ -35,37 +31,50 @@ def mock_input_dict_template(
 
 @pytest.fixture
 def patient_1_dict():
-    return mock_input_dict_template(
-        response_1="I get a headache.", response_7="a few seconds"
-    )
+    return mock_input_dict_template(response_1="I get a headache.")
 
 
 @pytest.fixture
 def patient_2_dict():
-    return {
-        "What other things do you experience right before or at the beginning of a seizure?": "",
-        "Please describe what you feel right before or at the beginning of a seizure.": "I feel pain, then I fall.",
-        "Please specify other warning.": "",
-        "Please specify other injuries.": "",
-        "What injuries have you experienced during a seizure?s": "",
-        "Please specify other symptoms.": "",
-        "Describe what happens during your seizures.": "",
-        "How long do your seizures last?": "a few seconds",
-    }
+    return mock_input_dict_template(response_1="I get dizzy.")
 
 
 @pytest.fixture
 def patient_3_dict():
-    return {
-        "What other things do you experience right before or at the beginning of a seizure?": "",
-        "Please describe what you feel right before or at the beginning of a seizure.": "",
-        "Please specify other warning.": "",
-        "Please specify other injuries.": "",
-        "What injuries have you experienced during a seizure?s": "",
-        "Please specify other symptoms.": "",
-        "Describe what happens during your seizures.": "",
-        "How long do your seizures last?": "a few seconds",
-    }
+    return mock_input_dict_template(response_1="I get a bit light headed.")
+
+
+@pytest.fixture
+def patient_4_dict():
+    return mock_input_dict_template(response_7="a few seconds")
+
+
+# @pytest.fixture
+# def patient_2_dict():
+#     return {
+#         "What other things do you experience right before or at the beginning of a seizure?": "",
+#         "Please describe what you feel right before or at the beginning of a seizure.": "I feel pain, then I fall.",
+#         "Please specify other warning.": "",
+#         "Please specify other injuries.": "",
+#         "What injuries have you experienced during a seizure?s": "",
+#         "Please specify other symptoms.": "",
+#         "Describe what happens during your seizures.": "",
+#         "How long do your seizures last?": "a few seconds",
+#     }
+
+
+# @pytest.fixture
+# def patient_3_dict():
+#     return {
+#         "What other things do you experience right before or at the beginning of a seizure?": "",
+#         "Please describe what you feel right before or at the beginning of a seizure.": "",
+#         "Please specify other warning.": "",
+#         "Please specify other injuries.": "",
+#         "What injuries have you experienced during a seizure?s": "",
+#         "Please specify other symptoms.": "",
+#         "Describe what happens during your seizures.": "",
+#         "How long do your seizures last?": "a few seconds",
+#     }
 
 
 class TestGetInputValues:
@@ -74,32 +83,37 @@ class TestGetInputValues:
     no answer respectively.
     """
 
+    nlp = spacy.load("en_core_web_sm")
+
     @pytest.mark.parametrize(
-        "mock_patient_dict, expected_boolean",
+        "mock_response_dict, expected_result",
         [
             (pytest.lazy_fixture("patient_1_dict"), False),
             (pytest.lazy_fixture("patient_2_dict"), True),
-            (pytest.lazy_fixture("patient_3_dict"), None),
+            (pytest.lazy_fixture("patient_3_dict"), True),
+            (pytest.lazy_fixture("patient_4_dict"), None),
         ],
     )
-    def test_get_input_values(self, mock_patient_dict, expected_boolean):
-        input_filter = InputFilter(patient_dict=mock_patient_dict)
-        result = input_filter.get_flag_value(
-            list_of_keys=BEFORE_EVENT_QUESTIONS, list_of_keywords=FLAG_6_KEYWORDS_BEFORE
+    def test_matches_input_1_criteria(self, mock_response_dict, expected_result):
+
+        result = matches_criteria(
+            nlp=self.nlp,
+            response_dict=mock_response_dict["patient_id"],
+            keywords_dict=KEYWORDS_DICT[0],
         )
 
-        assert result == expected_boolean
+        assert result == expected_result
 
 
-class TestTransformInput:
-    """Test for transform_input() function where a dictionary
-    of patients' questions and answers are transformed to a one-hot
-    encoded array.
-    """
+# class TestTransformInput:
+#     """Test for transform_input() function where a dictionary
+#     of patients' questions and answers are transformed to a one-hot
+#     encoded array.
+#     """
 
-    def test_transform_input(self, mock_input_dict):
+#     def test_transform_input(self, mock_input_dict):
 
-        result = transform_input(input_dict=mock_input_dict)
-        expected = np.array([[0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0]]).astype(float)
+#         result = transform_input(input_dict=mock_input_dict)
+#         expected = np.array([[0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0]]).astype(float)
 
-        np.testing.assert_array_equal(result, expected)
+#         np.testing.assert_array_equal(result, expected)
