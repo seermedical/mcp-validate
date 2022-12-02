bdaa1a7786b0027804e9285533a344d148aa1451
Dominique Eden <dominique@seermedical.com>
2022-12-02 15:10:41 +1100

    Add unit tests for metrics

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytest
 
 from src.generate_inputs import matches_criteria, KEYWORDS_DICT, transform_input
+from src.metrics import get_inputs_by_diagnosis, get_input_counts, get_metrics
 
 
 def mock_input_dict_template(
@@ -72,6 +73,54 @@ def patient_7_dict():
         response_1="Usually when I go to the toilet.",
         response_6="Blacking out.",
     )
+
+
+@pytest.fixture
+def mock_input_dict():
+    return {
+        mock_input_dict_template(
+            patient_id="patient_8",
+            response_0="I go pale, I get a headache.",
+            response_6="I droop.",
+        )[
+            0
+        ],  # non-epilepsy
+        mock_input_dict_template(
+            patient_id="patient_9",
+            response_0="I'm not sure",
+            response_1="I'm not sure",
+            response_6="I'm not sure",
+            response_7="a few seconds",
+        )[
+            0
+        ],  # epilepsy
+        mock_input_dict_template(
+            patient_id="patient_10", response_0="Some text", response_1="Some text"
+        )[
+            0
+        ],  # indeterminate
+    }
+
+
+@pytest.fixture
+def mock_input_array():
+    return np.array(
+        [
+            [1, 0, 1, np.nan, 1, 0],  # epilepsy
+            [0, 0, 0, 0, 0, 0],  # non-epilepsy
+            [0, np.nan, np.nan, np.nan, 0, np.nan],  # indeterminate
+        ]
+    )
+
+
+@pytest.fixture
+def mock_true_array():
+    return np.array([[0, 0, 1, 0, 0, 0], [0, 1, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0]])
+
+
+@pytest.fixture
+def mock_pred_array():
+    return np.array([[0, 0, 1, 0, 0, 0], [0, 1, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0]])
 
 
 class TestGetInputValues:
@@ -157,3 +206,116 @@ class TestTransformInput:
         expected = np.array([expected_output]).astype(float)
 
         np.testing.assert_array_equal(result, expected)
+
+
+class TestMetrics:
+    """Tests function to compute and return metrics."""
+
+    def test_get_inputs_by_diagnosis(self, mock_input_array, mock_true_array):
+
+        result = get_inputs_by_diagnosis(mock_input_array, mock_true_array)
+
+        np.testing.assert_array_equal(result[0], mock_input_array[[2]])
+        np.testing.assert_array_equal(result[1], mock_input_array[[1]])
+        np.testing.assert_array_equal(result[2], mock_input_array[[0]])
+
+    @pytest.mark.parametrize(
+        "input_idx, expected_result",
+        [
+            (
+                0,
+                {
+                    "indeterminate": {0: 1, 1: 0, np.nan: 0},
+                    "non_epilepsy": {0: 1, 1: 0, np.nan: 0},
+                    "epilepsy": {0: 0, 1: 1, np.nan: 0},
+                },
+            ),
+            (
+                1,
+                {
+                    "indeterminate": {0: 0, 1: 0, np.nan: 1},
+                    "non_epilepsy": {0: 1, 1: 0, np.nan: 0},
+                    "epilepsy": {0: 1, 1: 0, np.nan: 0},
+                },
+            ),
+        ],
+    )
+    def test_get_input_counts(self, input_idx, expected_result, mock_input_array):
+
+        input_arrays = (
+            mock_input_array[[2]],
+            mock_input_array[[1]],
+            mock_input_array[[0]],
+        )
+        result = get_input_counts(input_arrays, input_idx)
+        assert result == expected_result
+
+    def test_get_metrics(self, mock_input_array, mock_pred_array, mock_true_array):
+        result = get_metrics(mock_input_array, mock_pred_array, mock_true_array)
+        expected = {
+            "Name": "Evaluation 1",
+            "Description": "Metrics for Epilepsy vs Non-Epilepsy classes.",
+            "Summary": {
+                "total": {"predicted": 3, "true": 3},
+                "total_classes": {
+                    "predicted": 6,
+                    "true": 6,
+                },
+            },
+            "Counts": {
+                "responses": {},
+                "inputs": {
+                    0: {
+                        "indeterminate": {0: 1, 1: 0, np.nan: 0},
+                        "non_epilepsy": {0: 1, 1: 0, np.nan: 0},
+                        "epilepsy": {0: 0, 1: 1, np.nan: 0},
+                    },
+                    1: {
+                        "indeterminate": {0: 0, 1: 0, np.nan: 1},
+                        "non_epilepsy": {0: 1, 1: 0, np.nan: 0},
+                        "epilepsy": {0: 1, 1: 0, np.nan: 0},
+                    },
+                    2: {
+                        "indeterminate": {0: 0, 1: 0, np.nan: 1},
+                        "non_epilepsy": {0: 1, 1: 0, np.nan: 0},
+                        "epilepsy": {0: 0, 1: 1, np.nan: 0},
+                    },
+                    3: {
+                        "indeterminate": {0: 0, 1: 0, np.nan: 1},
+                        "non_epilepsy": {0: 1, 1: 0, np.nan: 0},
+                        "epilepsy": {0: 0, 1: 0, np.nan: 1},
+                    },
+                    4: {
+                        "indeterminate": {0: 1, 1: 0, np.nan: 0},
+                        "non_epilepsy": {0: 1, 1: 0, np.nan: 0},
+                        "epilepsy": {0: 0, 1: 1, np.nan: 0},
+                    },
+                    5: {
+                        "indeterminate": {0: 0, 1: 0, np.nan: 1},
+                        "non_epilepsy": {0: 1, 1: 0, np.nan: 0},
+                        "epilepsy": {0: 1, 1: 0, np.nan: 0},
+                    },
+                },
+                "diagnoses": {
+                    "predicted": {
+                        "indeterminate": 1,
+                        "non_epilepsy": 1,
+                        "epilepsy": 1,
+                    },
+                    "true": {
+                        "indeterminate": 1,
+                        "non_epilepsy": 1,
+                        "epilepsy": 1,
+                    },
+                },
+            },
+            "Performance": {
+                "accuracy": {
+                    "total": 3,
+                    "percentage": 1.0,
+                },
+                "accuracy_balanced": {"total": 1.0},
+            },
+        }
+
+        assert result == expected
