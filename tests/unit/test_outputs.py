0dec02072cd7a829c847df5d1673f4c7e364d84c
Dominique Eden <dominique@seermedical.com>
2022-12-15 13:48:37 +1100

    Init tests for generating output arrays

--- tests/unit/test_outputs.py
+++ tests/unit/test_outputs.py
@@ -1,1 +1,16 @@
-# TODO: Add output tests
+class TestGetOutputValues:
+    """Tests functions set_diagnosis(), set_single_diagnosis(),
+    get_predicted_output(), and get_true_output().
+    """
+
+    def test_set_diagnosis(self):
+        return
+
+    def test_set_single_diagnosis(self):
+        return
+
+    def test_get_predicted_output(self):
+        return
+
+    def test_get_true_output(self):
+        return
