3db03d4fe590c8e9dfaf1ba7686373edf75d4a83
Dominique Eden <dominique@seermedical.com>
2023-05-02 13:25:34 +1000

    Values encoded to floats for JSON serialization

--- src/metrics.py
+++ src/metrics.py
@@ -108,7 +108,7 @@ def get_accuracy(
         score = accuracy_score(
             y_true=true_labels, y_pred=pred_labels, normalize=normalize
         )
-    return score
+    return float(score)
 
 
 def get_labels(output_array: np.ndarray) -> np.ndarray:
@@ -140,10 +140,9 @@ def get_metrics(
     performance metrics.
 
     Args:
-        input_dict: Input data of patient's responses.
         input_array: Input data to model, represented by flags.
-        predicted_array: Predicted output of patient's diagnoses.
-        true_array: True output of patient's diagnoses.
+        pred_output: Predicted output of patient's diagnoses.
+        true_output: True output of patient's diagnoses.
 
     Returns:
         dict: A dictionary of summary and performance statistics.
@@ -156,10 +155,13 @@ def get_metrics(
         "Name": "Evaluation 1",
         "Description": "Metrics for Epilepsy vs Non-Epilepsy classes.",
         "Summary": {
-            "total": {"predicted": pred_output.shape[0], "true": true_output.shape[0]},
+            "total": {
+                "predicted": float(pred_output.shape[0]),
+                "true": float(true_output.shape[0]),
+            },
             "total_classes": {
-                "predicted": pred_output.shape[1],
-                "true": true_output.shape[1],
+                "predicted": float(pred_output.shape[1]),
+                "true": float(true_output.shape[1]),
             },
         },
         "Counts": {
@@ -167,14 +169,14 @@ def get_metrics(
             "inputs": {},
             "diagnoses": {
                 "predicted": {
-                    "indeterminate": pred_labels[0],
-                    "non_epilepsy": pred_labels[1],
-                    "epilepsy": pred_labels[2],
+                    "indeterminate": float(pred_labels[0]),
+                    "non_epilepsy": float(pred_labels[1]),
+                    "epilepsy": float(pred_labels[2]),
                 },
                 "true": {
-                    "indeterminate": true_labels[0],
-                    "non_epilepsy": true_labels[1],
-                    "epilepsy": true_labels[2],
+                    "indeterminate": float(true_labels[0]),
+                    "non_epilepsy": float(true_labels[1]),
+                    "epilepsy": float(true_labels[2]),
                 },
             },
         },
